4f1157df92ad56b0969fa80e6d16e3e6d49df89e
Vipin <vipin.jain@yash.com>
2018-05-14 11:35:25 +0530

    Added Close button and detail activity title

--- app/src/main/java/com/yash/ocr/DetailActivity.java
+++ app/src/main/java/com/yash/ocr/DetailActivity.java
@@ -2,9 +2,14 @@ package com.yash.ocr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailActivity extends Activity {
+
+    Button close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,14 @@ public class DetailActivity extends Activity {
         String data = getIntent().getStringExtra("DATA");
         TextView txt = findViewById(R.id.dataTxt);
         txt.setText(data);
+        txt.setMovementMethod(new ScrollingMovementMethod());
+        close = findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetailActivity.this.finish();
+            }
+        });
 
     }
 }
