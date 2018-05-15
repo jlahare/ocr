875d48a5e9e5b91cae176ea651976bbdbd95c4c0
Vipin <vipin.jain@yash.com>
2018-05-15 12:44:09 +0530

    update code

--- app/src/main/java/com/yash/ocr/DetailActivity.java
+++ app/src/main/java/com/yash/ocr/DetailActivity.java
@@ -6,6 +6,7 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailActivity extends Activity {
 
@@ -34,40 +35,47 @@ public class DetailActivity extends Activity {
         setInformation(data);
     }
 
-    private void setInformation(String[] data)
-    {
-        String bankName = data[0];
-        String cardNumber = "";
-        String txnNumber = "";
-        String amount = "";
+    private void setInformation(String[] data) {
+        try {
+            String bankName = data[0];
+            String cardNumber = "";
+            String txnNumber = "";
+            String amount = "";
 
+            for (int i = 0; i < data.length; i++) {
+                if (data[i].toLowerCase().indexOf("txn") != -1) {
+                    txnNumber = data[i];
+                    txnNumber = txnNumber.replace("TXN NO", "").trim();
+                    txnNumber = txnNumber.replace("TXN", "").trim();
+                    txnNumber = txnNumber.replace(":", "");
+                    txnNumber = txnNumber.replace("RESP", "").trim();
+                    txnNumber = txnNumber.replace("NO", "").trim();
+                    txnNumber = txnNumber.replace("NO", "").trim();
+                    txnNumber = txnNumber.replace("000", "").trim();
+                }
 
-        for(int i =0 ; i<data.length ; i++)
-        {
-            if(data[i].toLowerCase().indexOf("txn") !=-1)
-            {
-                txnNumber = data[i];
-                txnNumber = txnNumber.replace("TXN NO" , "");
-                txnNumber = txnNumber.replace(":" , "");
+                if (data[i].toLowerCase().indexOf("card") != -1) {
+                    cardNumber = data[i];
+                    cardNumber = cardNumber.replace(":", "");
+                    cardNumber = cardNumber.replace("CARD NO", "").trim();
+                }
+                if (data[i].toLowerCase().indexOf("unt") != -1) {
+                    amount = data[i];
+                    amount = amount.replace("AMOUNT", "").trim();
+                    amount = amount.replace("AMT", "").trim();
+                    amount = amount.replace("ALMOUNT", "").trim();
+                    amount = amount.replace(":", "");
+                }
             }
 
-            if(data[i].toLowerCase().indexOf("card") !=-1)
-            {
-                cardNumber = data[i];
-                cardNumber = cardNumber.replace(":" , "");
-                cardNumber = cardNumber.replace("CARD NO" , "").trim();
-            }
-            if(data[i].toLowerCase().indexOf("unt") !=-1)
-            {
-                amount = data[i];
-                amount = amount.replace("AMOUNT" , "").trim();
-            }
+            ((TextView) findViewById(R.id.address)).setText(bankName);
+            ((TextView) findViewById(R.id.card_no)).setText(cardNumber);
+            ((TextView) findViewById(R.id.txn_no)).setText(txnNumber);
+            ((TextView) findViewById(R.id.amount)).setText(amount);
+        } catch (Throwable throwable) {
+            Toast.makeText(getApplicationContext(), "Please try again", Toast.LENGTH_LONG).show();
         }
 
-        ((TextView)findViewById(R.id.address)).setText(bankName);
-        ((TextView)findViewById(R.id.card_no)).setText(cardNumber);
-        ((TextView)findViewById(R.id.txn_no)).setText(txnNumber);
-        ((TextView)findViewById(R.id.amount)).setText(amount);
 
     }
 }
